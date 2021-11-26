e6d098ae441e51e7d738f87d21dd7acad510524c
谭佳成 <uniqueacheng@gmail.com>
2021-11-26 13:48:08 +0800

    转义响应格式代码

--- lib/Server.go
+++ lib/Server.go
@@ -14,6 +14,13 @@ type sdkHttpServer struct {
 	Name string
 }
 
+// 响应json格式
+type commonResponse struct {
+	BizCode int
+	Msg string
+	Data interface{}
+}
+
 func (s *sdkHttpServer) Route(pattern string,handlerFunc func(c *Context)) {
 	http.HandleFunc(pattern, func(writer http.ResponseWriter, request *http.Request) {
 		handlerFunc(&Context{
@@ -34,3 +41,12 @@ func NewServer(name string) *sdkHttpServer{
 		Name: name,
 	}
 }
+
+func NewRes(code int,msg string,data interface{}) *commonResponse{
+	return &commonResponse{
+		BizCode: code,
+		Msg: msg,
+		Data: data,
+	}
+}
+
--- main.go
+++ main.go
@@ -16,34 +16,16 @@ func SignUp(c *web.Context) {
 	req := &signUpReq{}
 	err := c.ReadJson(req)
 	if err != nil {
-		c.SysErrJson(&commonResponse{
-			BizCode: 4,
-			Msg: fmt.Sprintf("error:%v",err),
-		})
+		c.SysErrJson(web.NewRes(500,fmt.Sprintf("error:%v",err),nil))
 		return
 	}
 
 	// 假设返回正确id
-	c.OkJson(&commonResponse{
-		BizCode: 1,
-		Msg: "success",
-		Data: 1,
-	})
-
-}
-
-
-
-// 响应json格式
-type commonResponse struct {
-	BizCode int
-	Msg string
-	Data interface{}
+	c.OkJson(web.NewRes(200,"success",1))
 }
 
 func main() {
 	server := web.NewServer("openresty")
-
 	server.Route("/signup",SignUp)
 	server.Start(":8099")
 }
